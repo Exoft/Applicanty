37d25651bbbd57dcb3a9d18372dd9d616a7fe456
Mykhailo1993 <33718769+Mykhailo1993@users.noreply.github.com>
2017-12-01 00:18:32 +0200

    updated UI project: added clarity UI theme. created sample login page. Added backend project

--- src/Applicant.UI/Controllers/HomeController.cs
+++ src/Applicant.UI/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Applicant.Controllers
+namespace Applicant.UI.Controllers
 {
     public class HomeController : Controller
     {
