5a69eae7aa1bc350ad201c7df273fcc9b4800ff2
Mykhailo1993 <33718769+Mykhailo1993@users.noreply.github.com>
2017-12-16 19:00:00 +0200

    Added a request for dynamically updating the page of candidates

--- src/Applicanty.UI/ClientApp/modules/candidates/components/candidates-list/candidates-list.component.ts
+++ src/Applicanty.UI/ClientApp/modules/candidates/components/candidates-list/candidates-list.component.ts
@@ -1,56 +1,63 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+import { CandidatesDataService } from '../../services/candidates-data.service';
 
 @Component({
     templateUrl: './candidates-list.component.html',
-    styleUrls: ['./candidates-list.component.scss']
+    styleUrls: ['./candidates-list.component.scss'],
+    providers: [CandidatesDataService]
 })
-export class CandidatesListComponent {
+export class CandidatesListComponent implements OnInit {
+    candidates = [];
 
-    candidates: any[] = [
-        {
-            name: 'Ivan',
-            experienceLevel: 'Jun',
-            lastVacancy: 'ASP.NET',
-            email: 'Ivan@gmail.com',
-            linkedIn: 'In',
-            phone: '+49 75 6778 5643',
-            age: 20
-        },
-        {
-            name: 'Taras',
-            experienceLevel: 'Midl',
-            lastVacancy: 'Frontend Engineer',
-            email: 'Taras@gmail.com',
-            linkedIn: 'In',
-            phone: '+49 56 6797 4576',
-            age: 21
-        },
-        {
-            name: 'Mikhaylo',
-            experienceLevel: 'Sin',
-            lastVacancy: 'Front-end',
-            email: 'Mikhaylo@gmail.com',
-            linkedIn: 'In',
-            phone: '+49 67 4578 5467',
-            age: 22
-        },
-        {
-            name: 'Bogdan',
-            experienceLevel: 'Jun',
-            lastVacancy: 'Web UI Software Engineer',
-            email: 'Bogdan@gmail.com',
-            linkedIn: 'In',
-            phone: '+49 96 6580 5469',
-            age: 23
-        },
-        {
-            name: 'Igor',
-            experienceLevel: 'Midl',
-            lastVacancy: 'Full Stack',
-            email: 'Igor@gmail.com',
-            linkedIn: 'In',
-            phone: '+49 09 8478 8934',
-            age: 24
-        }
-    ];
+    constructor(private http: CandidatesDataService) { }
+    ngOnInit() {
+        this.http.getCandidates().subscribe(data => this.candidates = data);
+    }
+    //candidates: any[] = [
+    //    {
+    //        name: 'Ivan',
+    //        experienceLevel: 'Jun',
+    //        lastVacancy: 'ASP.NET',
+    //        email: 'Ivan@gmail.com',
+    //        linkedIn: 'In',
+    //        phone: '+49 75 6778 5643',
+    //        age: 20
+    //    },
+    //    {
+    //        name: 'Taras',
+    //        experienceLevel: 'Midl',
+    //        lastVacancy: 'Frontend Engineer',
+    //        email: 'Taras@gmail.com',
+    //        linkedIn: 'In',
+    //        phone: '+49 56 6797 4576',
+    //        age: 21
+    //    },
+    //    {
+    //        name: 'Mikhaylo',
+    //        experienceLevel: 'Sin',
+    //        lastVacancy: 'Front-end',
+    //        email: 'Mikhaylo@gmail.com',
+    //        linkedIn: 'In',
+    //        phone: '+49 67 4578 5467',
+    //        age: 22
+    //    },
+    //    {
+    //        name: 'Bogdan',
+    //        experienceLevel: 'Jun',
+    //        lastVacancy: 'Web UI Software Engineer',
+    //        email: 'Bogdan@gmail.com',
+    //        linkedIn: 'In',
+    //        phone: '+49 96 6580 5469',
+    //        age: 23
+    //    },
+    //    {
+    //        name: 'Igor',
+    //        experienceLevel: 'Midl',
+    //        lastVacancy: 'Full Stack',
+    //        email: 'Igor@gmail.com',
+    //        linkedIn: 'In',
+    //        phone: '+49 09 8478 8934',
+    //        age: 24
+    //    }
+    //];
 }
--- src/Applicanty.UI/ClientApp/modules/candidates/services/candidates-data.service.ts
+++ src/Applicanty.UI/ClientApp/modules/candidates/services/candidates-data.service.ts
@@ -1,5 +1,7 @@
 ﻿import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 import { AuthService } from '../../../services/auth.service';
 
@@ -9,23 +11,23 @@ export class CandidatesDataService {
         private authService: AuthService) {
     }
     
-    public getCandidates() {
+    public getCandidates(): Observable<any> {
         return this.http.get('http://localhost:8000/api/candidate', { headers: this.authService.getAuthenticationHeader() });
     }
 
-    public getCandidate(candidateId: any) {
+    public getCandidate(candidateId: any): Observable<any> {
         return this.http.get('http://localhost:8000/api/candidate/' + candidateId, { headers: this.authService.getAuthenticationHeader() });
     }
 
-    public createCandidate(candidate: any) {
+    public createCandidate(candidate: any): Observable<any> {
         return this.http.post('http://localhost:8000/api/candidate', candidate, { headers: this.authService.getAuthenticationHeader() });
     }
 
-    public updateCandidate(candidate: any) {
+    public updateCandidate(candidate : any): Observable<any> {
         return this.http.put('http://localhost:8000/api/candidate', candidate, { headers: this.authService.getAuthenticationHeader() });
     }
 
-    public deleteCandidate(candidateId: any) {
+    public deleteCandidate(candidateId: any): Observable<any> {
         return this.http.delete('http://localhost:8000/api/candidate/' + candidateId, { headers: this.authService.getAuthenticationHeader() });
     }
 }
