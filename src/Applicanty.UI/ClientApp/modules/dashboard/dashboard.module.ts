ab7f1aa141f8f7ffce87d29014579831c443c2d3
Mykhailo Kurta <mkurta@exoft.net>
2017-12-27 11:27:40 +0200

    feature/add_empty_dashboard_component

---
+++ src/Applicanty.UI/ClientApp/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,22 @@
+﻿import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { ClarityModule } from 'clarity-angular';
+import { DashboardComponent } from '../dashboard/components/dashboard.component';
+import { AuthGuard } from '../../services/authguard.service';
+
+export const dashboardRoutes = [
+    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] }
+];
+
+@NgModule({
+    declarations: [
+        DashboardComponent
+    ],
+    imports: [
+        BrowserModule,
+        ClarityModule.forRoot()
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class DashboardModule {}
