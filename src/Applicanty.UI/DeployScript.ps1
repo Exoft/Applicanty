e1d842195835d1c41aea3e6b297aca6c9389b401
bsakalo <37376338+bsakalo@users.noreply.github.com>
2018-06-15 11:45:23 +0300

    Update DeployScript.ps1

--- src/Applicanty.UI/DeployScript.ps1
+++ src/Applicanty.UI/DeployScript.ps1
@@ -1,1 +1,8 @@
 $PSVersionTable.PSVersion
+
+net use Z: \\office.exoft.net\D$\apltest /user:exoft\Administrator M1llions2014
+dir Z:\
+#copy C:\Windows\win.ini Z:\
+dir Z:\
+net use
+net use * /delete
